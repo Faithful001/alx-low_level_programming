29eee01c56a9a15950b29b22c7511dfaa6eedf50
Faithful Eromosele <faithfuleromosele001@gmail.com>
2023-10-11 15:16:11 -0700

    Inital commit

---
+++ 0x02-functions_nested_loops/0-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+}
+
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+    char str[] = "_putchar\n";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    return 0;
+}
+
